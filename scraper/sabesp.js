9ea38f4a6d3cd050578901066b1b514505d39121
wakeupmh <wakeupmh@gmail.com>
2021-08-11 11:55:37 -0300

    refactor: add sabesp stealth mode

--- scraper/sabesp.js
+++ scraper/sabesp.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 const fillRGI = async (page, rgi) => {
   await page.waitForSelector('.w80VenEnd')
